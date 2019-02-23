bcd9c98b520dadc5dc813ad57e159a251bd4ad46
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-02-22 23:38:07 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,36 @@ const sass        = require('gulp-sass');
 const pug         = require('gulp-pug');
 const del         = require('del');
 const browserSync = require('browser-sync');
+const rename      = require('gulp-rename');
 const server      = browserSync.create();
+
+// Aux variables and functions
+function viewsPath(param) {
+  const path = 'src/views/';
+  return (typeof param === 'undefined') ? path : path + param;
+}
 
 // Tasks
 function clean() {
   return del(['dist']);
 }
 
-function build() {
-  series(clean);
+function build(done) {
+  series(clean, views)(done);
+}
+
+function views() {
+  return src(viewsPath('application.pug'))
+    .pipe(pug())
+    .pipe(rename('index.html'))
+    .pipe(dest('dist'));
 }
 
 // Server
+function watcher() {
+  watch(viewsPath('**/*.pug'), series(views, reload));
+}
+
 function reload(done) {
   server.reload();
   done();
@@ -25,16 +43,20 @@ function serve(done) {
     server: {
       baseDir: './dist',
     },
+
+    // Set 'false' to prevents browser automatically open when the server starts
     open: false,
   });
   done();
 }
 
 // Commands
-exports.clean = clean;
-exports.reload = reload;
-exports.serve = serve;
+exports.clean   = clean;
+exports.views   = views;
+exports.watcher = series(build, watcher);
+exports.reload  = reload;
+exports.serve   = serve;
 
 // Main Commands
-exports.build = series(clean);
-exports.dev = series(build, serve);
+exports.build   = series(clean, views);
+exports.dev     = series(build, serve, watcher);
