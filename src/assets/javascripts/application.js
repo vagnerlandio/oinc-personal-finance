334fd3d90c72d9bb4395e4794c8614310fcd6a1b
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2019-02-25 15:57:13 -0300

    Update url when clicked on sidenav buttons

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -9,3 +9,71 @@ sidenavLinks.forEach(function(elem) {
     event.preventDefault();
   });
 });
+
+let overview = document.querySelector('#overview');
+let accounts = document.querySelector('#accounts');
+let transactions = document.querySelector('#transactions');
+let categories = document.querySelector('#categories');
+let settings = document.querySelector('#settings');
+let about = document.querySelector('#about');
+
+overview.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    '/'
+  );
+});
+accounts.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    'accounts'
+  );
+});
+transactions.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    'transactions'
+  );
+});
+categories.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    'categories'
+  );
+});
+settings.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    'settings'
+  );
+});
+about.addEventListener('click', function() {
+  window.history.pushState(
+    // State
+    {id: ''},
+    // Page title
+    'title',
+    // Url
+    'about'
+  );
+});
